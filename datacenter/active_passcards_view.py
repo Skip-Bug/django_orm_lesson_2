11368a10e933339def7a1d692aa5f36105e81046
Leonid Stoyko <gribojor@gmail.com>
2026-03-27 14:27:39 +0500

    Update active_passcards_view.py

--- datacenter/active_passcards_view.py
+++ datacenter/active_passcards_view.py
@@ -6,7 +6,6 @@ from django.shortcuts import render
 def active_passcards_view(request):
     # Программируем здесь
 
-    all_passcards = Passcard.objects.all()
     active_passcards = Passcard.objects.filter(is_active=True)
     context = {
         'active_passcards': active_passcards,  # люди с активными пропусками
