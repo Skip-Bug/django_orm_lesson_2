9009732744189c7f601554362a8c9b97d3f3a25a
Leonid Stoyko <gribojor@gmail.com>
2026-03-31 15:00:49 +0500

    Update storage_information_view.py

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -1,14 +1,18 @@
-from datacenter.models import Visit, Passcard
-from datacenter.models import format_duration, is_visit_long
+from datacenter.models import Visit
+from datacenter.models import format_duration, get_duration, is_visit_long
 from django.shortcuts import render
+from django.utils.timezone import localtime
 
 
 def storage_information_view(request):
-    non_closed_visits = []
     visits = Visit.objects.filter(leaved_at__isnull=True)
 
+    non_closed_visits = []
     for visit in visits:
-        duration, entered, is_long = is_visit_long(visit, minutes=60)
+        duration = get_duration(visit)
+        entered = localtime(visit.entered_at)
+        is_long = is_visit_long(visit, minutes=60)
+
         non_closed_visits.append(
             {
                 'who_entered': visit.passcard.owner_name,
